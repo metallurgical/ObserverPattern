99cfab52b7926c3665548068285941425c0e0220
Norlihazmey Ghazali <norlihazmey89@gmail.com>
2017-04-19 13:10:15 +0800

    Update LoggingObserver.php

--- LoggingObserver.php
+++ LoggingObserver.php
@@ -4,7 +4,11 @@
 
 class LoggingObserver implements SplObserver {
 
+   // implement update method provided by SplObserver
   public function update ( SplSubject $splSubject ) {
+    // at here we can do what we want
+    // maybe want to sending an email
+    // or anything else
     echo 'New user with the name ' . $splSubject->getUserName() . '<br />';
   }
   
